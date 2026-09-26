b3440ebf13be7f70d6358765294ff99fbc273ae7
RishiBuilds <rishi.chaurasia.dev@gmail.com>
2026-09-26 19:46:58 +0530

    adding a discount sort option

--- src/lib/sorts.ts
+++ src/lib/sorts.ts
@@ -5,5 +5,6 @@ export const SORTS = {
   rating: "Top rated",
   price_asc: "Price: low to high",
   price_desc: "Price: high to low",
+  discount: "Biggest discount",
 } as const;
 export type SortKey = keyof typeof SORTS;
